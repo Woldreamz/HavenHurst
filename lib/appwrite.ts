5c1fac27dc41fa2b15ecc8b988a73132528819aa
Rainer Nsa <woldreamz@gmail.com>
2024-12-23 04:39:02 +0100

    Initialize AppWrite

---
+++ lib/appwrite.ts
@@ -0,0 +1,16 @@
+import {Avatars, Client} from "react-native-appwrite";
+
+export const config = {
+    platform: 'com.wdr.havenhurst',
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+}
+
+export const client = new Client();
+
+client
+    .setEndpoint(config.endpoint!)
+    .setProject(config.projectId!)
+    .setPlatform(config.platform!);
+
+export const avatar = new Avatars(client);
