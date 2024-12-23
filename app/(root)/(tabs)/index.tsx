bb7ce48ef5946a79addac4855e3164b784af588b
Rainer Nsa <woldreamz@gmail.com>
2024-12-23 03:13:26 +0100

    Nav Links

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="font-bold text-lg my-10">Welcome to HavenHurst</Text>
+      <Text className="font-bold my-10 font-rubik text-3xl">Welcome to HavenHurst</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
