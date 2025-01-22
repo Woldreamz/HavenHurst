834bd062fee3a9460a2389d788c5382afd13c531
Rainer Nsa <woldreamz@gmail.com>
2025-01-22 06:52:32 +0100

    Sign In update

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,6 +1,16 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import icons from "@/constants/icons";
+
+const TabIcon = ({ focused, icon, title }: { focused: boolean, icon: any, title: string}) => (
+  <View className="flex-1 mt-3 flex flex-col items-center">
+    <Image source={icon} tintColor={focused ? "#0061ff" : '#666876'} resizeMode="contain" className="size-7" />
+    <Text className={`${focused ? 'text-primary-300 font-rubik-medium' : 'text-black-200 font-rubik'} text-xs w-full text-center mt-1`}>
+      {title}
+    </Text>
+  </View>
+)
 
 const TabsLayout = () => {
   return (
@@ -21,10 +31,28 @@ const TabsLayout = () => {
          options={{
             title: 'Home',
             headerShown: false,
-            tabBarIcon: () => (
-                <View>
-                    <Text>Home</Text>
-                </View>
+            tabBarIcon: ({focused}) => (
+                <TabIcon icon={icons.home} focused={focused} title="Home" />
+            )
+         }}
+      />
+      <Tabs.Screen 
+         name="explore"
+         options={{
+            title: 'Explore',
+            headerShown: false,
+            tabBarIcon: ({focused}) => (
+                <TabIcon icon={icons.search} focused={focused} title="Explore" />
+            )
+         }}
+      />
+      <Tabs.Screen 
+         name="profile"
+         options={{
+            title: 'Profile',
+            headerShown: false,
+            tabBarIcon: ({focused}) => (
+                <TabIcon icon={icons.person} focused={focused} title="Profile" />
             )
          }}
       />
--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,11 +10,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="font-bold my-10 font-rubik text-3xl">Welcome to HavenHurst</Text>
-      <Link href="/sign-in">Sign In</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/properties/1">Property</Link>
+      <Text className="font-bold my-10 font-rubik text-3xl">
+        Welcome to HavenHurst
+      </Text>
     </View>
   );
 }
